af363c80b925af2695f900f7b13402f35c389a14
Kevin Tamarus Tang <kevintamarus@gmail.com>
2017-11-12 23:45:34 -0800

    gameplay components hooked up, game states added

--- src/components/GameScreen.js
+++ src/components/GameScreen.js
@@ -1,14 +1,35 @@
 import React, {Component} from 'react';
 
-class GameScreen extends Component {
+import CardsDisplay from './gameplay_components/CardsDisplay';
+import GameButtons from './gameplay_components/GameButtons';
+import CoinsDisplay from './gameplay_components/CoinsDisplay';
+import PlayerItems from './gameplay_components/PlayerItems';
 
-  render() {
-    return (
-      <div>
-        This is the Game Screen
-      </div>
-    )
+export default class GameScreen extends Component {
+  
+    constructor(props) {
+      super(props);
+      this.state = {
+        whiteCoins: 0,
+        blueCoins: 0,
+        greenCoins: 0,
+        redCoins: 0,
+        blackCoins: 0,
+        goldCoins: 5,
+        LvlOneCards: [],
+        LvlTwoCards: [],
+        LvlThreeCards: []
+      }
+    }
+  
+    render() {
+      return (
+        <div>
+          <CardsDisplay/>
+          <CoinDisplay/>
+          <PlayerItems/>
+          <GameButtons/>
+        </div>
+      )
+    }
   }
-}
-
-export default GameScreen;
