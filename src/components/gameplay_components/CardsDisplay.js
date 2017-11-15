e899a75b04035a2ee8d97c09d4f363a6d35cc913
Kevin Tamarus Tang <kevintamarus@gmail.com>
2017-11-15 00:40:06 -0800

    cards and nobles passed in, randomizing logic added

--- src/components/gameplay_components/CardsDisplay.js
+++ src/components/gameplay_components/CardsDisplay.js
@@ -1,6 +1,20 @@
 import React, {Component} from 'react';
 
 class CardsDisplay extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      levelOneDisplay: [],
+      levelTwoDisplay: [],
+      levelThreeDisplay: []
+    }
+  }
+
+  componentDidMount() {
+    this.setState({levelOneDisplay: this.props.levelOneCards.splice(0,4)});
+    this.setState({levelTwoDisplay: this.props.levelTwoCards.splice(0,4)});
+    this.setState({levelThreeDisplay: this.props.levelThreeCards.splice(0,4)});
+  }
 
   render() {
     return (
---
+++ src/components/gameplay_components/NoblesDisplay.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class NoblesDisplay extends Component {
+
+  render() {
+    return (
+      <div>
+        Nobles Display
+      </div>
+    )
+  }
+}
+
+export default NoblesDisplay;
