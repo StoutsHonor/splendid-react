e4591e4271f06be472df8d2add59a004b3040220
Kevin Tamarus Tang <kevintamarus@gmail.com>
2017-11-15 00:45:50 -0800

    import routes fixed, json file error fixed, renders properly

--- src/components/GameScreen.js
+++ src/components/GameScreen.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
+import NoblesDisplay from './gameplay_components/NoblesDisplay';
 import CardsDisplay from './gameplay_components/CardsDisplay';
 import GameButtons from './gameplay_components/GameButtons';
 import CoinsDisplay from './gameplay_components/CoinsDisplay';
 import PlayerItems from './gameplay_components/PlayerItems';
-import nobles from './json_files/nobles';
-import levelOneCards from './json_files/levelOneCards';
-import levelTwoCards from './json_files/levelOneCards';
-import levelThreeCards from './json_files/levelOneCards';
+import nobles from '../json_files/nobles';
+import levelOneCards from '../json_files/levelOneCards';
+import levelTwoCards from '../json_files/levelTwoCards';
+import levelThreeCards from '../json_files/levelThreeCards';
 import Random from 'react-randomizer';
 
 export default class GameScreen extends Component {
