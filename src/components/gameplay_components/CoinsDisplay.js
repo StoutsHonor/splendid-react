871bf696760b78bbabf8215c2c1d7aa9d492305d
Kevin Tamarus Tang <kevintamarus@gmail.com>
2017-11-20 01:07:30 -0800

    coin logic almost done, need to fix some edge scenarios

--- src/components/gameplay_components/CoinsDisplay.js
+++ src/components/gameplay_components/CoinsDisplay.js
@@ -6,8 +6,10 @@ class CoinsDisplay extends Component {
     super(props);
     this.toggleButtonsOn = this.toggleButtonsOn.bind(this);
     this.toggleButtonsOff = this.toggleButtonsOff.bind(this);
+    this.updateSelectedCoins = this.updateSelectedCoins.bind(this);
     this.state = {
-      showButtons: false
+      showButtons: false,
+      selectedCoins: []
     }
   }
 
@@ -19,28 +21,48 @@ class CoinsDisplay extends Component {
     this.props.isPlayerTurn ? this.setState({showButtons: false}) : null;
   }
 
+  updateSelectedCoins(color) {
+    let collect = true;
+    if(this.state.selectedCoins.length < 3) {
+      //taking 2 coins of same color
+      if(collect) {
+        if(this.state.selectedCoins.includes(color) && collect) {
+          collect = false;
+        }
+      }
+      this.setState({selectedCoins: this.state.selectedCoins.concat(color)});
+    }
+  }
+
   render() {
+    console.log(this.state.selectedCoins, 'selected coins')
     return (
       <div>
         <div>
           <Panel
             style={{cursor:'pointer'}}
-            onClick={this.toggleButtonsOn} 
+            onClick={() => { this.toggleButtonsOn;
+              this.updateSelectedCoins('white');
+            }} 
             className="col-sm-2 text-center" 
             header={<i className="fa fa-bandcamp fa-2x" style={{color:'gray'}}/>}>
             {this.props.whiteCoins}
           </Panel>
           <Panel
             style={{cursor:'pointer'}}
-            onClick={this.toggleButtonsOn} 
+            onClick={() => { this.toggleButtonsOn;
+              this.updateSelectedCoins('blue');
+            }} 
             className="col-sm-2 text-center"
             header={<i className="fa fa-bandcamp fa-2x" style={{color:'blue'}}/>}
-            sStyle="primary">
+            bsStyle="primary">
             {this.props.blueCoins}
           </Panel>
           <Panel
             style={{cursor:'pointer'}}
-            onClick={this.toggleButtonsOn} 
+            onClick={() => { this.toggleButtonsOn;
+              this.updateSelectedCoins('green');
+            }} 
             className="col-sm-2 text-center" 
             header={<i className="fa fa-bandcamp fa-2x" style={{color:'green'}}/>}
             bsStyle="success">
@@ -48,14 +70,19 @@ class CoinsDisplay extends Component {
           </Panel>
           <Panel
             style={{cursor:'pointer'}}
-            onClick={this.toggleButtonsOn} 
+            onClick={() => { this.toggleButtonsOn;
+              this.updateSelectedCoins('red');
+            }} 
             className="col-sm-2 text-center" 
-            header={<i className="fa fa-bandcamp fa-2x" style={{color:'red'}}/>}            bsStyle="danger">
+            header={<i className="fa fa-bandcamp fa-2x" style={{color:'red'}}/>}
+            bsStyle="danger">
             {this.props.redCoins}
           </Panel>
           <Panel
             style={{cursor:'pointer'}}
-            onClick={this.toggleButtonsOn} 
+            onClick={() => { this.toggleButtonsOn;
+              this.updateSelectedCoins('black');
+            }} 
             className="col-sm-2 text-center" 
             header={<i className="fa fa-bandcamp fa-2x" style={{color:'black'}}/>} 
             bsStyle="default">
@@ -63,14 +90,20 @@ class CoinsDisplay extends Component {
           </Panel>
           <Panel
             style={{cursor:'pointer'}}
-            onClick={this.toggleButtonsOn} 
+            onClick={() => alert("To collect a Gold Coin, please reserve a Card")} 
             className="col-sm-2 text-center" 
             header={<i className="fa fa-bandcamp fa-2x" style={{color:'#DAA520'}}/>}
             bsStyle="warning">
             {this.props.goldCoins}
           </Panel>
         </div>
-        
+        { this.state.selectedCoins.length > 0 ?
+          <div>
+            {this.state.selectedCoins.map((value, key) => {
+              return <i className="fa fa-bandcamp fa-2x" style={{color: value}} key={key}/>
+            })}
+          </div>: null
+        }
         { this.state.showButtons && this.props.isPlayerTurn ?
           <div className="text-center">
             <ButtonGroup>
