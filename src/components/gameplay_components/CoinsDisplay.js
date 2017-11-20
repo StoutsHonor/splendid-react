297d692d66423408ee711db9b29b53088b4dcc80
Kevin Tamarus Tang <kevintamarus@gmail.com>
2017-11-20 01:26:13 -0800

    only coin decrement and confirm button modify left for coin logic

--- src/components/gameplay_components/CoinsDisplay.js
+++ src/components/gameplay_components/CoinsDisplay.js
@@ -14,35 +14,42 @@ class CoinsDisplay extends Component {
   }
 
   toggleButtonsOn() {
+    console.log('clicked')
     this.props.isPlayerTurn ? this.setState({showButtons: true}) : null;
   }
 
   toggleButtonsOff() {
-    this.props.isPlayerTurn ? this.setState({showButtons: false}) : null;
+    this.setState({selectedCoins: []});
+    this.setState({showButtons: false});
   }
 
   updateSelectedCoins(color) {
-    let collect = true;
-    if(this.state.selectedCoins.length < 3) {
-      //taking 2 coins of same color
-      if(collect) {
-        if(this.state.selectedCoins.includes(color) && collect) {
-          collect = false;
+    let coins = this.state.selectedCoins;
+    const duplicateCheck = (a) => {
+      for(let i = 0; i < a.length; i++) {
+        for(let x = i; x < a.length; x++) {
+          if(x !== i && a[x] === a[i]) {
+            return true
+          }
         }
+        return false
       }
-      this.setState({selectedCoins: this.state.selectedCoins.concat(color)});
+    }
+    if(coins.length < 3 && !duplicateCheck(coins)) {
+      if(!coins.includes(color) || coins.length < 2) {
+        this.setState({selectedCoins: coins.concat(color)});
+      }
     }
   }
 
   render() {
-    console.log(this.state.selectedCoins, 'selected coins')
     return (
       <div>
         <div>
           <Panel
             style={{cursor:'pointer'}}
-            onClick={() => { this.toggleButtonsOn;
-              this.updateSelectedCoins('white');
+            onClick={() => { this.toggleButtonsOn();
+              this.updateSelectedCoins('gray');
             }} 
             className="col-sm-2 text-center" 
             header={<i className="fa fa-bandcamp fa-2x" style={{color:'gray'}}/>}>
@@ -50,7 +57,7 @@ class CoinsDisplay extends Component {
           </Panel>
           <Panel
             style={{cursor:'pointer'}}
-            onClick={() => { this.toggleButtonsOn;
+            onClick={() => { this.toggleButtonsOn();
               this.updateSelectedCoins('blue');
             }} 
             className="col-sm-2 text-center"
@@ -60,7 +67,7 @@ class CoinsDisplay extends Component {
           </Panel>
           <Panel
             style={{cursor:'pointer'}}
-            onClick={() => { this.toggleButtonsOn;
+            onClick={() => { this.toggleButtonsOn();
               this.updateSelectedCoins('green');
             }} 
             className="col-sm-2 text-center" 
@@ -70,7 +77,7 @@ class CoinsDisplay extends Component {
           </Panel>
           <Panel
             style={{cursor:'pointer'}}
-            onClick={() => { this.toggleButtonsOn;
+            onClick={() => { this.toggleButtonsOn();
               this.updateSelectedCoins('red');
             }} 
             className="col-sm-2 text-center" 
@@ -80,7 +87,7 @@ class CoinsDisplay extends Component {
           </Panel>
           <Panel
             style={{cursor:'pointer'}}
-            onClick={() => { this.toggleButtonsOn;
+            onClick={() => { this.toggleButtonsOn();
               this.updateSelectedCoins('black');
             }} 
             className="col-sm-2 text-center" 
@@ -98,9 +105,9 @@ class CoinsDisplay extends Component {
           </Panel>
         </div>
         { this.state.selectedCoins.length > 0 ?
-          <div>
+          <div className="text-center">
             {this.state.selectedCoins.map((value, key) => {
-              return <i className="fa fa-bandcamp fa-2x" style={{color: value}} key={key}/>
+              return <i className="fa fa-bandcamp fa-4x" style={{color: value}} key={key}/>
             })}
           </div>: null
         }
