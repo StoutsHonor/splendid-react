2c78f68042c37c815c2737dade43c7e68c95e073
Kevin Tamarus Tang <kevintamarus@gmail.com>
2017-11-23 12:40:33 -0800

    mvp card rendering completed

--- src/components/gameplay_components/Card.js
+++ src/components/gameplay_components/Card.js
@@ -30,17 +30,17 @@ export default class Card extends Component {
         header={
           <div>
             <i className="fa fa-superpowers fa-3x"/>
-            <span className='pull-right'>{this.props.card.points}</span>
+            {this.props.card.points ? <i className="fa fa-bolt fa-2x pull-right">{this.props.card.points}</i> : null }
           </div>
         } 
         bsStyle={this.convertStyle(this.props.card.persist)} 
         style={{backgroundColor: this.convertColor(this.props.card.persist), height: 250}}>
         <div>
-          {this.props.card.white? <div><i className="fa fa-bandcamp fa-2x" style={{color:'gray'}}>{this.props.card.white}</i></div> : null }
-          {this.props.card.blue? <div><i className="fa fa-bandcamp fa-2x" style={{color:'blue'}}>{this.props.card.blue}</i></div> : null }
-          {this.props.card.green? <div><i className="fa fa-bandcamp fa-2x" style={{color:'green'}}>{this.props.card.green}</i></div> : null }
-          {this.props.card.red? <div><i className="fa fa-bandcamp fa-2x" style={{color:'red'}}>{this.props.card.red}</i></div> : null }
-          {this.props.card.black? <div><i className="fa fa-bandcamp fa-2x" style={{color:'black'}}>{this.props.card.black}</i></div> : null }
+          {this.props.card.white ? <div><i className="fa fa-bandcamp fa-2x" style={{color:'gray'}}>{this.props.card.white}</i></div> : null }
+          {this.props.card.blue ? <div><i className="fa fa-bandcamp fa-2x" style={{color:'blue'}}>{this.props.card.blue}</i></div> : null }
+          {this.props.card.green ? <div><i className="fa fa-bandcamp fa-2x" style={{color:'green'}}>{this.props.card.green}</i></div> : null }
+          {this.props.card.red ? <div><i className="fa fa-bandcamp fa-2x" style={{color:'red'}}>{this.props.card.red}</i></div> : null }
+          {this.props.card.black ? <div><i className="fa fa-bandcamp fa-2x" style={{color:'black'}}>{this.props.card.black}</i></div> : null }
         </div>
       </Panel>
     )
