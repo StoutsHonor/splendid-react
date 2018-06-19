7042cd840cc97eac3360cf927b68201d677cbedf
Kevin Tamarus Tang <kevintamarus@gmail.com>
2018-06-18 23:20:31 -0700

    fixed ternary statements on card component

--- src/components/shared/Card.js
+++ src/components/shared/Card.js
@@ -21,17 +21,17 @@ export default class Card extends Component {
               header={
                 <div>
                   <i className="fa fa-eercast fa-3x" style={{color: persistColor}}/>
-                  {this.props.card.points ? <i className="fa fa-bolt fa-2x pull-right" style={{color: '#DAA520'}}>{this.props.card.points}</i> : null }
+                  {this.props.card.points && <i className="fa fa-bolt fa-2x pull-right" style={{color: '#DAA520'}}>{this.props.card.points}</i>}
                 </div>
               }
               bsStyle={convertStyle(this.props.card.persist)}
               style={{backgroundColor: convertColor(this.props.card.persist), height: 250, marginLeft: 10, cursor: 'pointer'}}>
               <div>
-                {this.props.card.white ? <div><i className="fa fa-bandcamp fa-2x" style={{color:'gray'}}>{this.props.card.white}</i></div> : null }
-                {this.props.card.blue ? <div><i className="fa fa-bandcamp fa-2x" style={{color:'blue'}}>{this.props.card.blue}</i></div> : null }
-                {this.props.card.green ? <div><i className="fa fa-bandcamp fa-2x" style={{color:'green'}}>{this.props.card.green}</i></div> : null }
-                {this.props.card.red ? <div><i className="fa fa-bandcamp fa-2x" style={{color:'red'}}>{this.props.card.red}</i></div> : null }
-                {this.props.card.black ? <div><i className="fa fa-bandcamp fa-2x" style={{color:'black'}}>{this.props.card.black}</i></div> : null }
+                {this.props.card.white && <div><i className="fa fa-bandcamp fa-2x" style={{color:'gray'}}>{this.props.card.white}</i></div>}
+                {this.props.card.blue && <div><i className="fa fa-bandcamp fa-2x" style={{color:'blue'}}>{this.props.card.blue}</i></div>}
+                {this.props.card.green && <div><i className="fa fa-bandcamp fa-2x" style={{color:'green'}}>{this.props.card.green}</i></div>}
+                {this.props.card.red && <div><i className="fa fa-bandcamp fa-2x" style={{color:'red'}}>{this.props.card.red}</i></div>}
+                {this.props.card.black && <div><i className="fa fa-bandcamp fa-2x" style={{color:'black'}}>{this.props.card.black}</i></div>}
               </div>
             </Panel>
           )
